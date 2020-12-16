8e8cab5221f7a6c7fcf40de93c0557d45152afde
Nicolas Bayet <nicolas.bayet@gmail.com>
2020-12-16 15:57:17 +0100

    temp

--- src/main.ts
+++ src/main.ts
@@ -56,10 +56,6 @@ enum KeyChangeState {
   PRESS = 1,
   REPEAT = 2,
 }
-/**
- * Encaplusale key RELEASE and PRESS for more expressive statements.
- */
-const keyChangeBinaryState = new Set([KeyChangeState.RELEASE, KeyChangeState.PRESS]);
 
 const execUID = 1000;
 const execGID = 1000;
@@ -70,6 +66,8 @@ const execGID = 1000;
 //     commands: string[];
 //   };
 // }
+
+// fs.watch(
 
 function compileKeymap(config: Config, activeLayers: string[]): Record<string, KeyDefinition> {
   const compiledLayouts = new Set<string>();
@@ -118,17 +116,23 @@ function compileKeymap(config: Config, activeLayers: string[]): Record<string, K
       keysDefinitions[key] = keyDefinition;
     }
   }
+  // for (const definition of Object.values(keysDefinitions)) {
+  //   definition.keymaps = definition.keymaps.reverse();
+  // }
   return keysDefinitions;
 }
 
 const keyStates: Record<string, boolean> = {};
 const layerState: Record<string, boolean> = {};
 /**
- * In case we active a layer, increment a counter for a particular key.
- * It is possbile that multiples logic activate the same layer at the same time.
- * The number represent how many different logic currently activate the.
+ * A map of keycode that must be released when a particural code is pressed.
+ *
+ * For instance, if a key has been remaped in a particular layer, if the key is
+ * not released before releasing the layer, storing the key to release is
+ * required.
  */
-const preventKey: Record<string, number> = {};
+const remapToRelease: Record<number, Set<number>> = {};
+
 const defaultDeviceName = 'GoaTouch virtual device';
 
 (async () => {
@@ -140,11 +144,8 @@ const defaultDeviceName = 'GoaTouch virtual device';
     const keyboardConfig = getKeyboard(config, device.name);
     if (!keyboardConfig) continue;
 
-    console.log();
-    console.log('--------------------');
-    console.log('name::', device.name);
-    // console.log('keyboardConfig:', keyboardConfig);
-    // console.log('layout', config.layouts);
+    console.log('Remapping device:', device.name);
+
     const layout = getLayout(config, keyboardConfig.layout);
     if (!layout) {
       throw new Error(
@@ -153,49 +154,37 @@ const defaultDeviceName = 'GoaTouch virtual device';
     }
     device.listen();
     device.grab();
-    const activeLayers = Object.entries(layerState)
-      .map(([layerName, state]) => layerName && state && layerName)
-      .filter((layerName) => !!layerName);
-    let compiledKeymap = compileKeymap(config, [keyboardConfig.layout, ...activeLayers]);
+
+    const getActiveLayer = () => {
+      const activeLayers = Object.entries(layerState)
+        .map(([layerName, state]) => layerName && state && layerName)
+        .filter((layerName) => !!layerName);
+      return [keyboardConfig.layout, ...activeLayers];
+    };
+    let compiledKeymap = compileKeymap(config, getActiveLayer());
 
     device.on('event', (event) => {
-      // if (event.value !== KeyChangeState.REPEAT) console.log('layerState:', layerState);
-
       const type = eventTypes[event.type];
       const linuxKeyNames = type.events[event.code];
       if (type.name === 'EV_KEY') {
-        // console.log('event:', event);
         const keyNames = new Set<string>();
         const currentKeymaps: SimpleKeymap[] = [];
 
         const visitedKeys = new Set<string>();
         const visitStack = [...linuxKeyNames];
         let currentKey: string;
-        // const definitions: KeyDefinition[] = [];
-        // console.log('visitStack:', visitStack);
-        // console.log(
-        //   'compiledKeymap:',
-        //   Object.keys(compiledKeymap).map((key) => ({
-        //     ...compiledKeymap[key],
-        //     key: key,
-        //     keymaps: JSON.stringify(compiledKeymap[key].keymaps),
-        //   })),
-        // );
         while ((currentKey = visitStack.shift())) {
           keyNames.add(currentKey);
           visitedKeys.add(currentKey);
           const keyDefinition = compiledKeymap[currentKey];
           if (!keyDefinition) continue;
-          // definitions.push(keyDefinition);
           for (const alias of keyDefinition.aliases) {
             if (!visitedKeys.has(alias)) {
               visitStack.push(alias);
             }
           }
-          // console.log('keyDefinition:', keyDefinition);
           currentKeymaps.push(...keyDefinition.keymaps);
         }
-        // console.log('compiledKeymap:', compiledKeymap);
 
         for (const keyName of keyNames) {
           keyStates[keyName] = !!event.value;
@@ -208,73 +197,68 @@ const defaultDeviceName = 'GoaTouch virtual device';
           }
         }
 
-        if (event.value !== KeyChangeState.REPEAT) {
-          // console.log('event.value:', event.value);
-          // console.log('keyNames:', keyNames);
-          // console.log('currentKeymaps:', currentKeymaps);
-        }
         let commandFound = false;
+        if (event.value === KeyChangeState.RELEASE) {
+          const releaseCodes = remapToRelease[event.code];
+          if (releaseCodes) {
+            console.log('releaseCodes:', releaseCodes);
+            for (const code of releaseCodes) {
+              const nevEvent: NevEvent = {
+                type: constants.EV_KEY,
+                code: code,
+                value: KeyChangeState.RELEASE,
+              };
+              const keyNames = eventTypes[constants.EV_KEY].events[code];
+              console.log('send reselease for key', keyNames);
+              uinput.write(nevEvent);
+              uinput.sync();
+            }
+          }
+          remapToRelease[event.code] = undefined;
+        }
         for (const keymap of currentKeymaps) {
-          if (keymap.commands.length && keymap.modifiers.every((m) => keyStates[m])) {
-            commandFound = true;
-
+          if (keymap.commands.length) {
+            const allModifiersPressed = keymap.modifiers.every((m) => keyStates[m]);
             for (const command of keymap.commands) {
-              // check if command is conssidedred as a key remapping
-              console.log('command:', command);
+              // check if command is coniere as a key remapping
+              if (event.value !== KeyChangeState.REPEAT) {
+                console.log('command:', command);
+              }
               let match: RegExpMatchArray;
-              if (command.match(/^\s*\w+\s*$/)) {
+              if (allModifiersPressed && command.match(/^\s*\w+\s*$/)) {
                 //if (event.value === KeyChangeState.REPEAT) continue;
+                commandFound = true;
                 const key = command.trim();
                 console.log('changing command to ', key);
-                const code = constants[command.trim().toUpperCase()];
+                const remappedCode = constants[command.trim().toUpperCase()];
                 const nevEvent = {
                   type: constants['EV_KEY'],
-                  code: code,
-                  value: event.value,
+                  code: remappedCode,
+                  value: !allModifiersPressed ? 0 : event.value,
                 };
+                remapToRelease[event.code] = remapToRelease[event.code] || new Set();
+                remapToRelease[event.code].add(remappedCode);
                 uinput.write(nevEvent);
                 uinput.sync();
                 // check if we want to run a shell command
               } else if (
                 (match = command.match(/^\s*>(.*)/)) &&
-                event.value === KeyChangeState.PRESS
+                (event.value === KeyChangeState.PRESS || event.value === KeyChangeState.REPEAT) &&
+                allModifiersPressed
               ) {
+                commandFound = true;
                 const shellCommand = match[1];
                 console.log('calling a sub process:');
                 console.log(shellCommand);
-                exec(
-                  shellCommand,
-                  { uid: execUID, gid: execGID },
-                  // (error, stdout, stderr) => {
-                  //   console.log('############################################');
-                  //   console.log('result of command: ', command);
-                  //   if (error) {
-                  //     console.log('error:');
-                  //     console.log(error);
-                  //     console.log('');
-                  //   }
-                  //   console.log('stdout:');
-                  //   console.log(stdout);
-                  //   console.log('');
-                  //   if (stderr) {
-                  //     console.log('stderr:');
-                  //     console.log(stderr);
-                  //     console.log('');
-                  //   }
-                  //   console.log('############################################');
-                  // },
-                );
+                exec(shellCommand, { uid: execUID, gid: execGID });
 
                 // check if we want to activate/deactivate a layer
               } else if ((match = command.match(/^\s*#(.*)/))) {
+                commandFound = true;
                 if (event.value === KeyChangeState.REPEAT) continue;
-                // if (!keyChangeBinaryState.has(event.value)) continue;
                 const layerName = match[1].trim();
-                layerState[layerName] = !!event.value;
-                compiledKeymap = compileKeymap(config, [keyboardConfig.layout, ...activeLayers]);
-
-                // preventKey[event.code] = preventKey[event.code] || 0;
-                // preventKey[event.code] += event.value ? KeyChangeState.PRESS : -1;
+                layerState[layerName] = !!event.value && allModifiersPressed;
+                compiledKeymap = compileKeymap(config, getActiveLayer());
               }
             }
             break;
@@ -284,19 +268,7 @@ const defaultDeviceName = 'GoaTouch virtual device';
           uinput.write(event);
           uinput.sync();
         }
-        // uinput.write(event);
-        // uinput.sync();
       }
     });
-    // console.log('layout:', layout);
-    // console.log('keymap:', keymap);
-    // console.log('--------------------');
-    // console.log('name::', device.name);
-    // console.log('phys::', device.phys);
-    // console.log('id_product::', device.id_product);
-    // console.log('id_vendor::', device.id_vendor);
-    // console.log('id_bustype::', device.id_bustype);
-    // console.log('id_version::', device.id_version);
-    // console.log('driver_version::', device.driver_version);
   }
 })();
