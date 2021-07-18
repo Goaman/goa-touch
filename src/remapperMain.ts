f64644a3090db4fb335877c315ff9074f9f9e4f5
Nicolas Bayet <nicolas.bayet@gmail.com>
2021-07-18 15:25:24 +0200

    exit more gracefully

--- src/remapperMain.ts
+++ src/remapperMain.ts
@@ -2,5 +2,20 @@ import { Remapper } from './Remapper';
 export const start = async () => {
   const remapper = new Remapper();
   await remapper.start();
+
+  function handle() {
+    remapper.unbind();
+
+    process.off('SIGHUP', handle);
+    process.off('SIGQUIT', handle);
+    process.off('SIGINT', handle);
+    process.off('SIGTERM', handle);
+    process.exit(1);
+  }
+
+  process.on('SIGHUP', handle);
+  process.on('SIGQUIT', handle);
+  process.on('SIGINT', handle);
+  process.on('SIGTERM', handle);
 };
 start();
